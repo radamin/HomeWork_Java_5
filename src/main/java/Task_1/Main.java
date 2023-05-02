d7d860836327e0c76e07dc3945b392ae57608410
radamin <radamin@yandex.ru>
2023-05-02 22:07:02 +0300

    Task_1, Task_2, Task_3

--- src/main/java/Task_1/Main.java
+++ src/main/java/Task_1/Main.java
@@ -1,2 +1,27 @@
-package Task_1;public class Main {
+package Task_1;
+
+//Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1 человек может иметь несколько телефонов.
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Map<Integer, ArrayList<String>> phoneBook = new HashMap<>();
+        Map<Integer, String> nameBook = new HashMap<>();
+        nameBook.put(0, "Иванов А.А.");
+        nameBook.put(1, "Петров С.А.");
+        nameBook.put(2, "Иванов А.Ю.");
+        nameBook.put(3, "Сидоров А.К.");
+        for (int i=0;i< nameBook.size();i++){
+            if (phoneBook.get(i)==null){
+                phoneBook.put(i, new ArrayList<>());
+                phoneBook.get(i).add("8-965-454-00-"+i+i+";"+"8-920-855-01-"+i+i);
+            }
+            System.out.println("Телефонные номера абонента"+" "+nameBook.get(i)+" :"+phoneBook.get(i));
+        }
+
+    }
 }
--- src/main/java/Task_3/Main.java
+++ src/main/java/Task_3/Main.java
@@ -1,2 +1,44 @@
-package Task_3;public class Main {
+package Task_3;
+// На шахматной доске расставить 8 ферзей так, чтобы они не били друг друга
+public class Main {
+    public static void main(String[] args) {
+        int[] queens = new int[8];
+        findPosition(0, queens);
+
+        // Вывод в консоль
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                if (queens[row] == col) {
+                    System.out.print("X");
+                } else {
+                    System.out.print("o");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    // Функция для поиска ферзей
+    private static boolean findPosition(int row, int[] queens) {
+        if (row == 8) {
+            return true;
+        }
+        for (int col = 0; col < 8; col++) {
+            boolean isSafe = true;
+            for (int i = 0; i < row; i++) {
+                // Проверка на наличие конфликта
+                if (queens[i] == col || queens[i] == col - row + i || queens[i] == col + row - i) {
+                    isSafe = false;
+                    break;
+                }
+            }
+            if (isSafe) {
+                queens[row] = col; // Сохранить в массив
+                if (findPosition(row + 1, queens)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
